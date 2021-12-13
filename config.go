27e63a82d7b42cc74e99129449711d4e61966df0
Jonas Schmitt <utunq@student.kit.edu>
2021-12-13 14:08:06 +0100

    Add local config file support

---
+++ config.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io/fs"
+	"log"
+	"os"
+)
+
+const CONFIG_FILE = "config.json"
+
+type Configuration struct {
+	DBHost     string
+	DBToken    string
+	DBOrg      string
+	DBBucket   string
+	DefaultTTL int
+	Metrics    []MetricConfig
+	CacheSize  int
+	Prefetch   bool
+}
+
+type MetricConfig struct {
+	MetricType  string
+	Measurement string
+}
+
+func (c *Configuration) Init() {
+	data, err := os.ReadFile(CONFIG_FILE)
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		log.Fatalf("Could not read config file: %v\n Error: %v\n", CONFIG_FILE, err)
+	}
+	json.Unmarshal(data, c)
+}
