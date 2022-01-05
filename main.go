c8eb0bf4cdf00fcc67902e00546d22bb6a540cf4
Jonas Schmitt <utunq@student.kit.edu>
2022-01-05 09:39:36 +0100

    backend: Remove duplicated CORS statements

--- main.go
+++ main.go
@@ -22,10 +22,10 @@ var jobCache = LRUCache{}
 var authManager = AuthManager{}
 
 func JobStart(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	allowCors(r, w.Header())
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("Could not read http request body")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -34,7 +34,6 @@ func JobStart(w http.ResponseWriter, r *http.Request, params httprouter.Params) 
 	err = json.Unmarshal(body, &job)
 	if err != nil {
 		log.Printf("Could not unmarshal http request body")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -44,10 +43,10 @@ func JobStart(w http.ResponseWriter, r *http.Request, params httprouter.Params) 
 }
 
 func JobStop(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	allowCors(r, w.Header())
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("Could not read http request body")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -56,7 +55,6 @@ func JobStop(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	err = json.Unmarshal(body, &stopJob)
 	if err != nil {
 		log.Printf("Could not parse json from http request body")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -65,7 +63,6 @@ func JobStop(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := strconv.Atoi(strId)
 	if err != nil {
 		log.Printf("Id is not a valid integer")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -82,6 +79,7 @@ func JobStop(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 }
 
 func GetJobs(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	allowCors(r, w.Header())
 	// TODO: Check auth and get by e.g. project id, ...
 	// Get all for now
 	jobs := store.GetAll()
@@ -90,15 +88,14 @@ func GetJobs(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	data, err := json.Marshal(&jobListData)
 	if err != nil {
 		log.Printf("Could not marshal jobs to json")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	allowCors(r, w.Header())
 	w.Write(data)
 }
 
 func GetJob(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	allowCors(r, w.Header())
 	start := time.Now()
 
 	node := r.URL.Query().Get("node")
@@ -107,7 +104,6 @@ func GetJob(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	id, err := strconv.Atoi(strId)
 	if err != nil {
 		log.Printf("Could not job id data")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -117,7 +113,6 @@ func GetJob(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	job, err := store.Get(id)
 	if err != nil {
 		log.Printf("Could not get job meta data")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -130,7 +125,6 @@ func GetJob(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	}
 	if err != nil {
 		log.Printf("Could not get job metric data: %v\n", err)
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -138,7 +132,6 @@ func GetJob(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	jsonData, err := json.Marshal(&jobData)
 	if err != nil {
 		log.Printf("Could not marshal job to json")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -146,32 +139,30 @@ func GetJob(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	elapsed := time.Since(start)
 	log.Printf("Get job %v took %s", job.Id, elapsed)
 
-	allowCors(r, w.Header())
 	w.Write(jsonData)
 }
 
 func Search(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	allowCors(r, w.Header())
 	searchTerm := params.ByName("term")
 
 	id, err := strconv.Atoi(searchTerm)
 	if err == nil {
 		_, err := store.Get(id)
 		if err == nil {
-			allowCors(r, w.Header())
 			w.Write([]byte(fmt.Sprintf("job:%v", id)))
 			return
 		}
 	}
 
-	allowCors(r, w.Header())
 	w.Write([]byte(fmt.Sprintf("user:%v", searchTerm)))
 }
 
 func Login(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	allowCors(r, w.Header())
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("Could not read http request body")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -180,7 +171,6 @@ func Login(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	err = json.Unmarshal(body, &dat)
 	if err != nil {
 		log.Printf("Could not unmarshal http request body")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -188,7 +178,6 @@ func Login(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	user, err := authManager.AuthUser(dat.Username, dat.Password)
 	if err != nil {
 		log.Printf("Could not authenticate user: %v", err)
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
@@ -196,19 +185,17 @@ func Login(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	err = authManager.AppendJWT(user, dat.Remember, w)
 	if err != nil {
 		log.Printf("Could not generate JWT: %v", err)
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	allowCors(r, w.Header())
 	w.WriteHeader(200)
 }
 
 func Logout(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	allowCors(r, w.Header())
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Printf("Could not read http request body")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -217,33 +204,29 @@ func Logout(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	err = json.Unmarshal(body, &dat)
 	if err != nil {
 		log.Printf("Could not unmarshal http request body")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	if dat.Username == "" {
 		log.Printf("No username given on logout")
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	authManager.Logout(dat.Username)
 
-	allowCors(r, w.Header())
 	http.SetCookie(w, &http.Cookie{Name: "Authorization", Value: "", Expires: time.Unix(0, 0), Path: "/"})
 	w.WriteHeader(200)
 }
 
 func GenerateAPIKey(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	allowCors(r, w.Header())
 	jwt, err := authManager.GenerateJWT(UserInfo{Role: JOBCONTROL, Username: "api"}, true)
 	if err != nil {
 		log.Printf("Could not generate JWT: %v", err)
-		allowCors(r, w.Header())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	allowCors(r, w.Header())
 	w.Write([]byte(jwt))
 }
 
