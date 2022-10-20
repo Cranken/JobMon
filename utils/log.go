76bb5283d11d88111fbae227027446a013218c90
Jonas Schmitt <utunq@student.kit.edu>
2022-10-20 20:34:22 +0200

    backend: Add ring buffer to livelog

--- utils/log.go
+++ utils/log.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"container/ring"
 	"io"
 	"log"
 
@@ -9,15 +10,21 @@ import (
 
 type WebLogger struct {
 	defaultWriter io.Writer
-	connections []*websocket.Conn
+	connections   []*websocket.Conn
+	ring          *ring.Ring
 }
 
 func (l *WebLogger) Init() {
 	l.defaultWriter = log.Writer()
 	l.connections = make([]*websocket.Conn, 0)
+	l.ring = ring.New(25)
 }
 
 func (l *WebLogger) Write(p []byte) (n int, err error) {
+	// Copy to value to avoid accessing temporary variables
+	l.ring.Value = make([]byte, len(p))
+	copy(l.ring.Value.([]byte), p)
+	l.ring = l.ring.Next()
 	for _, c := range l.connections {
 		if err := c.WriteMessage(websocket.TextMessage, p); err != nil {
 			l.defaultWriter.Write([]byte(err.Error()))
@@ -27,6 +34,14 @@ func (l *WebLogger) Write(p []byte) (n int, err error) {
 }
 
 func (l *WebLogger) AddConnection(c *websocket.Conn) {
+	l.ring.Do(func(v any) {
+		if v != nil {
+			msg := v.([]byte)
+			if len(msg) > 0 {
+				c.WriteMessage(websocket.TextMessage, msg)
+			}
+		}
+	})
 	l.connections = append(l.connections, c)
 }
 
