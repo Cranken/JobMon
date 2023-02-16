970b730375a1c0f18e240f97a5b97455e80558fc
Holger Obermaier <holger.obermaier@kit.edu>
2023-02-16 13:12:49 +0100

    Add function to set log level

--- backend/logging/logging.go
+++ backend/logging/logging.go
@@ -1,45 +1,63 @@
 package logging
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 const (
-	error = iota
-	warning
-	info
-	debug
+	offLevel = iota
+	errorLevel
+	warningLevel
+	infoLevel
+	debugLevel
 )
 
 var (
-	logLevel int         = debug
+	// Logging level from off to debug
+	logLevel int = debugLevel
+
+	// Loggers for various logging levels
 	errorLog *log.Logger = log.New(os.Stderr, "ERROR ", log.LstdFlags)
 	warnLog  *log.Logger = log.New(os.Stderr, "WARN ", log.LstdFlags)
 	infoLog  *log.Logger = log.New(os.Stdout, "INFO ", log.LstdFlags)
 	debugLog *log.Logger = log.New(os.Stderr, "DEBUG ", log.LstdFlags)
 )
 
+// Error prints an error logging message
 func Error(e ...interface{}) {
-	if logLevel >= debug {
+	if logLevel <= errorLevel {
 		errorLog.Print(e...)
 	}
 }
 
+// Warning prints an warning logging message
 func Warning(e ...interface{}) {
-	if logLevel >= warning {
+	if logLevel <= warningLevel {
 		warnLog.Print(e...)
 	}
 }
 
+// Warning prints an information logging message
 func Info(e ...interface{}) {
-	if logLevel >= info {
+	if logLevel <= infoLevel {
 		infoLog.Print(e...)
 	}
 }
 
+// Debug prints a debug logging message
 func Debug(e ...interface{}) {
-	if logLevel >= debug {
+	if logLevel <= debugLevel {
 		debugLog.Print(e...)
 	}
+}
+
+// SetLogLevel sets the logging level between off an debugging
+func SetLogLevel(l int) error {
+	if l < offLevel || l > debugLevel {
+		return fmt.Errorf("log level must be between offLevel = %v and debugLevel = %v", offLevel, debugLevel)
+	}
+	logLevel = l
+	return nil
 }
