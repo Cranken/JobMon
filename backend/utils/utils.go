1a87553a8f8f19ca1210757fda1f4353dbbe1b62
Holger Obermaier <holger.obermaier@kit.edu>
2023-02-17 14:10:07 +0100

    Add comments

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -4,7 +4,8 @@ import (
 	"net/http"
 )
 
-// Helper function to allow CORS
+// Helper function to allow Cross-Origin Resource Sharing (CORS)
+// See: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 func AllowCors(r *http.Request, header http.Header) {
 	header.Set("Access-Control-Allow-Methods", r.Header.Get("Allow"))
 	header.Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
@@ -12,6 +13,7 @@ func AllowCors(r *http.Request, header http.Header) {
 	header.Set("Access-Control-Expose-Headers", "Set-Cookie")
 }
 
+// Contains checks if slice <slice> contains an element with value <val>
 func Contains[T comparable](slice []T, val T) bool {
 	for _, elem := range slice {
 		if val == elem {
@@ -21,6 +23,8 @@ func Contains[T comparable](slice []T, val T) bool {
 	return false
 }
 
+// Remove creates a new container with all elements of <container>
+// except first element with value <target>
 func Remove[T comparable](container []T, target T) []T {
 	for i, v := range container {
 		if v == target {
@@ -30,6 +34,7 @@ func Remove[T comparable](container []T, target T) []T {
 	return container
 }
 
+// Tuple with elements <First> and <Second>
 type Tuple[T any, V any] struct {
 	First  T
 	Second V
