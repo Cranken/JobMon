f277f2fdd295805a6dd7e3417fa1e4e521656f9a
Daniel Schild <utmwq@student.kit.edu>
2023-02-28 17:39:50 +0100

    Add JSDoc and some other comments to utils/ in the frontend

--- frontend/utils/auth.ts
+++ frontend/utils/auth.ts
@@ -2,17 +2,27 @@ import base64url from "base64url";
 import Cookies from "js-cookie";
 import { useCookies } from "react-cookie";
 
+/**
+ * UserRole defines roles for users
+ */
 export enum UserRole {
   Admin = "admin",
   User = "user",
   JobControl = "job-control",
 }
 
+/**
+ * AuthUser specifies an authenticated user with its username and its roles
+ */
 interface AuthUser {
   Username: string;
   Roles: UserRole[];
 }
 
+/**
+ * Constructs a AuthUser instance for the currently used user. The user is read from the browser's cookies.
+ * @returns The user as an AuthUser instance.
+ */
 export const useGetUser = () => {
   const [cookies] = useCookies(["Authorization"]);
   const authToken = cookies.Authorization as string;
@@ -27,12 +37,21 @@ export const useGetUser = () => {
   return { Username: data["Username"], Roles: data["Roles"] } as AuthUser;
 };
 
+/**
+ * Checks if the currently used user is valid. The user therefore is read from the browser's cookies.
+ * A user is seen as authenticated when his role and his username have a value unlike undefined.
+ */
 export const useIsAuthenticated = () => {
   const user = useGetUser();
   return user.Roles !== undefined && user.Username !== undefined;
 };
 
-
+/**
+ * Fetches data authenticated. This methode executes the request specified by url and options with included credentials.
+ * This method is useful when accessing protected areas in the backend.
+ * @param url The url to execute the request on.
+ * @param options The options for the request.
+ */
 export const authFetch = (url: string, options?: RequestInit) => {
   const p = fetch(url.toString(), { credentials: "include", ...options }).then((res) => {
     if (!res.ok && (res.status === 401 || res.status === 403)) {
--- frontend/utils/utils.ts
+++ frontend/utils/utils.ts
@@ -4,13 +4,38 @@ import { useCookies } from "react-cookie";
 import { DataMap, JobListData, JobTag } from "../types/job";
 import { JobSearchParams } from "../types/job";
 
+/**
+ * Clamps the value of val between min and max.
+ * @param val The original value.
+ * @param min The minimum value.
+ * @param max The maximum value.
+ * @returns The original val, in case it is contained in [min,max]. Otherwise, either to min or max value is returned.
+ */
 export const clamp = (val: number, min: number, max: number) =>
   Math.min(Math.max(val, min), max);
 
+/**
+ * Checks if point is between d1 and d2.
+ * @param d1 The lower limit.
+ * @param d2 The upper limit.
+ * @param point The point to check.
+ * @returns True if point is in [d1,d2].
+ */
 export function checkBetween<T>(d1: T, d2: T, point: T) {
   return d1 <= point && point <= d2;
 }
 
+/**
+ * Stores element in browser storage.
+ * Localstorage data has no expiration time, so the stored data is saved across browser sessions.
+ * @param key The key for the element.
+ * @param value The value to store.
+ * @returns Returns four elements,
+ *          the state of the stored element,
+ *          a function to modify the value,
+ *          a function to remove the item from the browser storage and
+ *          a state that indicates if browser storage was already checked for existing values.
+ */
 export function useStorageState<T>(
   key: string,
   value: T
@@ -32,6 +57,7 @@ export function useStorageState<T>(
     }
   };
 
+  //Check if item with the same key already exists in localstorage
   useEffect(() => {
     if (typeof window !== "undefined") {
       const saved = localStorage.getItem(key);
@@ -39,6 +65,8 @@ export function useStorageState<T>(
         const value = JSON.parse(saved) as T;
         setState(value);
       }
+
+      //Indicate that values are ready
       setIsLoading(false);
     }
   }, [key]);
@@ -46,6 +74,12 @@ export function useStorageState<T>(
   return [state, setStorageState, clearState, isLoading];
 }
 
+/**
+ * Fetches Jobs from the backend.
+ * @param params The parameters to restrict the jobs returned by the backend.
+ *                If no parameters are given, the method will not fetch from the backend.
+ * @returns A set of jobs returned from the backend and a state object indicating if the request to the backend is finished.
+ */
 export const useGetJobs: (
   params?: JobSearchParams
 ) => [JobListData | undefined, boolean] = (params?: JobSearchParams) => {
@@ -57,6 +91,8 @@ export const useGetJobs: (
     const url = new URL(
       "http://" + process.env.NEXT_PUBLIC_BACKEND_URL + "/api/jobs"
     );
+
+    //Parse params
     if (params) {
       Object.keys(params).forEach((val) => {
         if (val === "Tags") {
@@ -72,6 +108,8 @@ export const useGetJobs: (
     } else {
       return;
     }
+
+    //Fetching and parsing results from the backend
     fetch(url.toString(), {
       credentials: "include",
     }).then((res) => {
@@ -88,6 +126,12 @@ export const useGetJobs: (
   return [jobListData, isLoading];
 };
 
+/**
+ * Groups data with a key function.
+ * @param data The data to group.
+ * @param keyFunc The key function. The key function is used to generate keys for given objects.
+ * @returns Returns an array with all elements from data. Each element in the result has a key, determined with the key function.
+ */
 export function groupBy<T>(
   data: T[],
   keyFunc: (obj: T) => string
@@ -104,6 +148,11 @@ export function groupBy<T>(
   return groups;
 }
 
+/**
+ * Adds a tag to the given job.
+ * @param jobId The jobId is used to identify the job to which the tag should be added.
+ * @param tag The tag.
+ */
 export const addJobTag = (jobId: number, tag: string) => {
   const url = new URL(
     "http://" +
@@ -114,6 +163,11 @@ export const addJobTag = (jobId: number, tag: string) => {
   return setJobTag(url.toString(), { Name: tag } as JobTag);
 };
 
+/**
+ * Removes a tag from the given job.
+ * @param jobId The jobId is used to identify the job to from which the tag should be removed.
+ * @param tag The tag.
+ */
 export const removeJobTag = (jobId: number, tag: JobTag) => {
   const url = new URL(
     "http://" +
@@ -124,6 +178,11 @@ export const removeJobTag = (jobId: number, tag: JobTag) => {
   return setJobTag(url.toString(), tag);
 };
 
+/**
+ * Calls the backend api to modify tags.
+ * @param url The url to call. Additionally, to the backend's address the url specifies which operation will be executed.
+ * @param tag The tag.
+ */
 const setJobTag = (url: string, tag: JobTag) => {
   return fetch(url, {
     credentials: "include",
@@ -132,6 +191,11 @@ const setJobTag = (url: string, tag: JobTag) => {
   });
 };
 
+/**
+ * Converts a date-object into unix-time.
+ * @param d The date to convert.
+ * @returns The corresponding unix-time.
+ */
 export const dateToUnix = (d: Date) => {
   return parseInt((d.getTime() / 1000).toFixed(0));
 };
