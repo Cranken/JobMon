33aee825652ab0c0cd614b25420ea482681e4924
Holger Obermaier <holger.obermaier@kit.edu>
2023-03-20 18:08:25 +0100

    Create frontend development environment with docker

--- start_dev_env.sh
+++ start_dev_env.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-# Installe required node.js modules
-cd frontend
-if [[ -f yarn.lock ]]; then
-    yarn --frozen-lockfile
-elif [[ -f package-lock.json ]]; then
-     npm ci
-elif [ -f pnpm-lock.yaml ]; then
-     yarn global add pnpm && pnpm i --frozen-lockfile
-else
-    echo "Lockfile not found." && exit 1
-fi
-cd -
-
 # Build jobmon backend
-sudo docker build --pull --target dev_env --tag jobmon_backend_dev_env backend
+sudo docker build --pull --target dev_env --tag jobmon_backend_dev_env  backend
+
+# Build jobmon frontend
+sudo docker build --pull --target dev_env --tag jobmon_frontend_dev_env frontend
 
 # close previous sessions
 tmux kill-session -t jobmon_dev
@@ -31,7 +21,12 @@ tmux send -t jobmon_dev:0.0  \
     --mount=type=volume,source=jobmon_backend_cache_go_build,target=/root/.cache/go-build \
     --mount=type=bind,source=./,target=/app \
     jobmon_backend_dev_env" C-m
-tmux send -t jobmon_dev:0.1 "yarn dev" C-m
+tmux send -t jobmon_dev:0.1 "sudo docker run \
+    --interactive \
+    --tty \
+    --mount=type=bind,source=./,target=/app  \
+    --mount=type=volume,source=jobmon_frontend_nodes_modules,target=/app/node_modules \
+    jobmon_frontend_dev_env" C-m
 
 # Attache to the session
 tmux attach-session -t jobmon_dev
