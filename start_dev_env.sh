3008a169ec7b52e2aaa57ce3b0be24c6a2e2a283
Holger Obermaier <holger.obermaier@kit.edu>
2023-03-20 12:58:59 +0100

    Create golang development environment with docker

--- start_dev_env.sh
+++ start_dev_env.sh
@@ -13,6 +13,9 @@ else
 fi
 cd -
 
+# Build jobmon backend
+sudo docker build --pull --target dev_env --tag jobmon_backend_dev_env backend
+
 # close previous sessions
 tmux kill-session -t jobmon_dev
 
@@ -21,7 +24,13 @@ tmux new-session -c backend -d -s jobmon_dev
 tmux split-window -c frontend -h
 
 # Start jobmon frontend and backend in this session
-tmux send -t jobmon_dev:0.0 "go run ." C-m
+tmux send -t jobmon_dev:0.0  \
+"sudo docker run \
+    --interactive \
+    --tty \
+    --mount=type=volume,source=jobmon_backend_cache_go_build,target=/root/.cache/go-build \
+    --mount=type=bind,source=./,target=/app \
+    jobmon_backend_dev_env" C-m
 tmux send -t jobmon_dev:0.1 "yarn dev" C-m
 
 # Attache to the session
