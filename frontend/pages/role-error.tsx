0da60fed705302d0ba163fec37e3fc1d168c9039
Daniel Schild <utmwq@student.kit.edu>
2023-03-22 10:43:06 +0100

    Add basic role-error-page and modify header

--- frontend/pages/role-error.tsx
+++ frontend/pages/role-error.tsx
@@ -1,19 +1,61 @@
 import React from "react";
-import {Stack, Center, Text, Heading, useColorModeValue} from "@chakra-ui/react";
+import {Stack, Center, Text, Heading, useColorModeValue, Box, FormLabel, Input, Button} from "@chakra-ui/react";
+import {Field, Form, Formik} from "formik";
 
 const RoleError = () => {
     const borderColor = useColorModeValue("gray.500", "whiteAlpha.500");
-    return <Center>
-        <Stack borderWidth="1px" borderRadius="lg" p={5} margin={4} borderColor={borderColor}>
-            <Center>
-                <Heading>
-                    Access Denied
-                </Heading>
-            </Center>
-            <Text>
-                You need a appropriate role to access the frontend
-            </Text>
-        </Stack>
-    </Center>;
+    const submitRequest = (username: string, contactAddress: string, description: string) => {
+        console.log(username);
+        console.log(contactAddress);
+        console.log(description);
+    }
+    return <React.Fragment>
+        <Center>
+            <Stack borderWidth="1px" borderRadius="lg" p={5} margin={4} borderColor={borderColor}>
+                <Center>
+                    <Heading>
+                        This user is not permitted to access this webpage
+                    </Heading>
+                </Center>
+                <Center>
+                    <Text>
+                        To get access you need to get a role assigned by the administration team.
+                        You can request access with the form below
+                    </Text>
+                </Center>
+            </Stack>
+        </Center>
+        <Center>
+            <Stack borderWidth="1px" borderRadius="lg" p={5} margin={4} borderColor={borderColor}>
+                <Formik
+                    initialValues={{ username: "", contact: "", description: ""}}
+                    onSubmit={
+                        (values) => submitRequest(
+                            values.username,
+                            values.contact,
+                            values.description)}>
+                    <Form>
+                        <Stack direction="row">
+                            <Box>
+                                <FormLabel pt={1}>Username</FormLabel>
+                                <Field id="username" name="username" placeholder="" as={Input} />
+                            </Box>
+                            <Box>
+                                <FormLabel pt={1}>Username</FormLabel>
+                                <Field id="contact" name="contact" placeholder="" as={Input} />
+                            </Box>
+                            <Box>
+                                <FormLabel pt={1}>Username</FormLabel>
+                                <Field id="description" name="description" placeholder="" as={Input} />
+                            </Box>
+                            <Button type="submit" colorScheme="green" alignSelf="end">
+                                Submit Request
+                            </Button>
+                        </Stack>
+                    </Form>
+                </Formik>
+            </Stack>
+        </Center>
+    </React.Fragment>;
 }
 export default RoleError;
