dfef621637ebbc9e40edfc3bab5b6f8b98d66e75
Holger Obermaier <holger.obermaier@kit.edu>
2023-03-22 08:57:08 +0100

    Add function to clear development environment

--- start_development_env.sh
+++ start_development_env.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+function clear_dev_env() {
+    sudo docker system prune --all --volumes
+    sudo docker volume rm \
+        jobmon_frontend_nodes_modules \
+        jobmon_backend_cache_go_build \
+        jobmon_backend_cache_go_packages
+    echo "Docker images:"
+    sudo docker images ls
+    echo "Docker containers:"
+    sudo docker container ls
+    echo "Docker volumes:"
+    docker volume ls
+}
+
 # Rebuild development environment containers
 sudo docker compose --file docker-compose.development.yml build --pull
 
