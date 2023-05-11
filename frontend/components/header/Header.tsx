c7b9955ae5ee8a8f60b32a8fe891c10fd322da74
Daniel Schild <daniel.schild@student.kit.edu>
2023-05-11 17:03:39 +0200

    Makes the header switch to a drawer small devices

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -1,7 +1,13 @@
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
   Flex,
   Icon,
   Input,
@@ -10,14 +16,38 @@ import {
   Tooltip,
   useColorMode,
   useColorModeValue,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { useCookies } from "react-cookie";
 import { MdLogout } from "react-icons/md";
 import {useHasNoAllowedRole, useIsAuthenticated } from "@/utils/auth";
 import { useGetUser, UserRole } from "@/utils/user";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export const Header = () => {
+/**
+ * On devices with a lower width than defined here, the header will switch to the one defined as the small device header
+ */
+const SMALL_DEVICE_WIDTH_BOUNDARY = 700;
+
+/**
+ * Props for the header component
+ */
+interface HeaderProps {
+  /**
+   * The pathname on which the website currently renders
+   */
+  pathname: string
+}
+
+/**
+ * The header that gets displayed on each page.
+ * This header automatically switches the shown content based on the currently authenticated user.
+ * Depending on the width of the device this page automatically switches between two options.
+ * 
+ * @param param0 Properties
+ * @returns The component
+ */
+export const Header = ({pathname} : HeaderProps) => {
   const user = useGetUser();
   const { colorMode, toggleColorMode } = useColorMode();
   const [, , removeCookie] = useCookies(["Authorization"]);
@@ -26,6 +56,23 @@ export const Header = () => {
   const searchInputColor = useColorModeValue("gray.800", "whiteAlpha.900");
   const isAuthenticated = useIsAuthenticated();
   const hasRole = (user.Roles) ? !useHasNoAllowedRole(user) : false;
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [isWide, setIsWide] = useState(true);
+  const smallDeviceHeaderOpenButtonRef = React.useRef()
+
+  const updateWide = () => {
+    setIsWide( window.innerWidth > SMALL_DEVICE_WIDTH_BOUNDARY );
+  }
+
+  useEffect(() => {
+    // Gets executed on component mount
+    updateWide();
+    window.addEventListener("resize", updateWide)
+    return () => {
+      // gets executed on component unmount
+      window.removeEventListener("resize", updateWide)
+    }
+  })
 
   const logout = () => {
     fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/api/logout", {
@@ -35,6 +82,98 @@ export const Header = () => {
     }).then(() => removeCookie("Authorization", { path: "/" }));
   };
 
+  // Small device header
+  if (!isWide) {
+    return (
+      <header>
+        <Flex bg={headerBg} p={2}>
+          {isAuthenticated && hasRole ? (
+            <>
+              <Button bg={buttonBg} onClick={onOpen} mr={3}>
+                <HamburgerIcon />
+              </Button>
+              <Drawer
+                isOpen={isOpen}
+                placement='left'
+                onClose={onClose}
+              >
+                <DrawerOverlay />
+                <DrawerContent>
+                  <DrawerCloseButton />
+                  <DrawerHeader>Menu</DrawerHeader>
+                  <DrawerBody>
+                    <LinkBox>
+                      <LinkOverlay href="/jobs">
+                        <Button
+                          bg={buttonBg}
+                          w={"100%"}
+                          isActive={pathname == "/jobs"}
+                        >
+                          Jobs
+                        </Button>
+                      </LinkOverlay>
+                    </LinkBox>
+                    {user.Roles?.includes(UserRole.Admin) ?? false ? (
+                      <LinkBox mt={4}>
+                        <LinkOverlay href="/statistics">
+                          <Button
+                            bg={buttonBg}
+                            w={"100%"}
+                            isActive={pathname == "/statistics"}
+                          >
+                            Statistics
+                          </Button>
+                        </LinkOverlay>
+                      </LinkBox>
+                    ) : null}
+                    {user.Roles?.includes(UserRole.Admin) ?? false ? (
+                      <LinkBox mt={4}>
+                        <LinkOverlay href="/settings">
+                          <Button
+                            bg={buttonBg}
+                            w={"100%"}
+                            isActive={pathname == "/settings"}
+                          >
+                            Settings
+                          </Button>
+                        </LinkOverlay>
+                      </LinkBox>
+                    ) : null}
+                  </DrawerBody>
+                </DrawerContent>
+              </Drawer>
+            </>
+          ) : null }
+          <Box flexGrow={1}>
+            {isAuthenticated && hasRole ? (
+              <Input
+                placeholder="Search user/job"
+                onKeyPress={(ev) => searchHandler(ev.key, ev.currentTarget.value)}
+                borderColor={searchInputColor}
+                _placeholder={{ color: searchInputColor }}
+              />
+            ) : null}
+          </Box>
+          <Flex flexGrow={1} justify={"end"} gap={2} pl={2}>
+            <Tooltip label="Toggle Color Mode">
+              <Button bg={buttonBg} onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Tooltip>
+            {isAuthenticated ? (
+              <Tooltip label="Logout">
+                <Button bg={buttonBg} onClick={() => logout()}>
+                  <Icon as={MdLogout} />
+                </Button>
+              </Tooltip>
+            ) : null}
+          </Flex>
+        </Flex>
+      </header>
+    );
+  }
+
+  // Wide device header
   return (
     <header>
       <Flex bg={headerBg} p={2}>
@@ -43,20 +182,20 @@ export const Header = () => {
             <>
               <LinkBox>
                 <LinkOverlay href="/jobs">
-                  <Button bg={buttonBg}>Jobs</Button>
+                  <Button bg={buttonBg} isActive={pathname == "/jobs"}>Jobs</Button>
                 </LinkOverlay>
               </LinkBox>
               {user.Roles?.includes(UserRole.Admin) ?? false ? (
                 <LinkBox>
                   <LinkOverlay href="/statistics">
-                    <Button bg={buttonBg}>Statistics</Button>
+                    <Button bg={buttonBg} isActive={pathname == "/statistics"}>Statistics</Button>
                   </LinkOverlay>
                 </LinkBox>
               ) : null}
               {user.Roles?.includes(UserRole.Admin) ?? false ? (
                 <LinkBox>
                   <LinkOverlay href="/settings">
-                    <Button bg={buttonBg}>Settings</Button>
+                    <Button bg={buttonBg} isActive={pathname == "/settings"}>Settings</Button>
                   </LinkOverlay>
                 </LinkBox>
               ) : null}
