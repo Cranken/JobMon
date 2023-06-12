fbb020dc5ab606fe4f5d343832b52af221f2c64c
Daniel Schild <daniel.schild@student.kit.edu>
2023-06-12 15:05:08 +0200

    Adds search icon to the search-bar

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, MoonIcon, SearchIcon, SunIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
@@ -11,6 +11,8 @@ import {
   Flex,
   Icon,
   Input,
+  InputGroup,
+  InputRightElement,
   LinkBox,
   LinkOverlay,
   Tooltip,
@@ -20,7 +22,7 @@ import {
 } from "@chakra-ui/react";
 import { useCookies } from "react-cookie";
 import { MdLogout } from "react-icons/md";
-import {useHasNoAllowedRole, useIsAuthenticated } from "@/utils/auth";
+import { useHasNoAllowedRole, useIsAuthenticated } from "@/utils/auth";
 import { useGetUser, UserRole } from "@/utils/user";
 import React from "react";
 import { useIsWideDevice } from "@/utils/utils";
@@ -44,7 +46,7 @@ interface HeaderProps {
  * This path determines whether one of the links in the header shall be highlighted as the currently active page.
  * @returns The component
  */
-export const Header = ({pathname} : HeaderProps) => {
+export const Header = ({ pathname }: HeaderProps) => {
   const user = useGetUser();
   const { colorMode, toggleColorMode } = useColorMode();
   const [, , removeCookie] = useCookies(["Authorization"]);
@@ -55,6 +57,7 @@ export const Header = ({pathname} : HeaderProps) => {
   const hasRole = (user.Roles) ? !useHasNoAllowedRole(user) : false;
   const { isOpen, onOpen, onClose } = useDisclosure();
   const isWideDevice = useIsWideDevice();
+  const [searchValue, setSearchValue] = React.useState('')
 
   /**
    * Logout is a function called to to logout a user.
@@ -129,15 +132,27 @@ export const Header = ({pathname} : HeaderProps) => {
                 </DrawerContent>
               </Drawer>
             </>
-          ) : null }
+          ) : null}
           <Box flexGrow={1}>
             {isAuthenticated && hasRole ? (
-              <Input
-                placeholder="Search user/job"
-                onKeyPress={(ev) => searchHandler(ev.key, ev.currentTarget.value)}
-                borderColor={searchInputColor}
-                _placeholder={{ color: searchInputColor }}
-              />
+              <InputGroup>
+                <Input
+                  placeholder="Search user/job"
+                  onKeyDown={(event) => {
+                    if (event.key === "Enter") {
+                      search(searchValue)
+                    }
+                  }}
+                  onChange={(event) => setSearchValue(event.target.value)}
+                  borderColor={searchInputColor}
+                  _placeholder={{ color: searchInputColor }}
+                />
+                <InputRightElement width='4.5rem'>
+                  <Button h='1.75rem' size='sm' onClick={() => { search(searchValue) }} bg={buttonBg}>
+                    <SearchIcon />
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
             ) : null}
           </Box>
           <Flex flexGrow={1} justify={"end"} gap={2} pl={2}>
@@ -197,12 +212,24 @@ export const Header = ({pathname} : HeaderProps) => {
         </Flex>
         <Box flexGrow={1}>
           {isAuthenticated && hasRole ? (
-            <Input
-              placeholder="Search user/job"
-              onKeyPress={(ev) => searchHandler(ev.key, ev.currentTarget.value)}
-              borderColor={searchInputColor}
-              _placeholder={{ color: searchInputColor }}
-            />
+            <InputGroup>
+              <Input
+                placeholder="Search user/job"
+                onKeyDown={(event) => {
+                  if (event.key === "Enter") {
+                    search(searchValue)
+                  }
+                }}
+                onChange={(event) => setSearchValue(event.target.value)}
+                borderColor={searchInputColor}
+                _placeholder={{ color: searchInputColor }}
+              />
+              <InputRightElement width='4.5rem'>
+                <Button h='1.75rem' size='sm' onClick={() => { search(searchValue) }} bg={buttonBg}>
+                  <SearchIcon />
+                </Button>
+              </InputRightElement>
+            </InputGroup>
           ) : null}
         </Box>
         <Flex flexGrow={1} justify={"end"} gap={2}>
@@ -225,31 +252,30 @@ export const Header = ({pathname} : HeaderProps) => {
 };
 
 /**
- * This handler is called for changes in the search input-field.
- * @param key The key that caused the handler to get called.
- * @param term The term currently written in the input-field.
+ * This function searches for a given string
+ * The search is performed in the backend. Searches for jobs and users are supported.
+ * 
+ * @param term The term to search for.
  */
-const searchHandler = (key: string, term: string) => {
-  if (key === "Enter") {
-    fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/api/search/${term}`, {
-      credentials: "include",
-    }).then((res) =>
-      res.text().then((val) => {
-        const split = val.split(":");
-        if (split.length !== 2) {
+const search = (term: string) => {
+  fetch(process.env.NEXT_PUBLIC_BACKEND_URL + `/api/search/${term}`, {
+    credentials: "include",
+  }).then((res) =>
+    res.text().then((val) => {
+      const split = val.split(":");
+      if (split.length !== 2) {
+        return;
+      }
+      switch (split[0]) {
+        case "job":
+          window.location.href = `/job/${split[1]}`;
           return;
-        }
-        switch (split[0]) {
-          case "job":
-            window.location.href = `/job/${split[1]}`;
-            return;
-          case "user":
-            window.location.href = `/jobs?user=${split[1]}`;
-            return;
-        }
-      })
-    );
-  }
-};
+        case "user":
+          window.location.href = `/jobs?user=${split[1]}`;
+          return;
+      }
+    })
+  );
+}
 
 export default Header;
