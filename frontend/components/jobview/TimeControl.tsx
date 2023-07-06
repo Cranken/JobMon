7ee7fc7d7e29a1a795905fc8b4e175877cfdadde
Daniel Schild <daniel.schild@student.kit.edu>
2023-07-06 14:49:37 +0200

    Moves time control into modal on small devices

--- frontend/components/jobview/TimeControl.tsx
+++ frontend/components/jobview/TimeControl.tsx
@@ -1,7 +1,17 @@
 import {
   Box,
   Button,
+  Center,
   Flex,
+  Hide,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Show,
+  useDisclosure,
 } from "@chakra-ui/react";
 import Slider, { createSliderWithTooltip } from "rc-slider";
 import { JobMetadata } from "../../types/job";
@@ -42,6 +52,7 @@ const TimeControl = ({
     defaultTime[0],
     defaultTime[1],
   ]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     // Setting the text for the tooltip dynamically.
@@ -50,8 +61,12 @@ const TimeControl = ({
 
   const marks: Record<number, ReactNode> = {};
   let t = metadata.StartTime * 1000;
-  const inc = ((metadata.StopTime - metadata.StartTime) / 4) * 1000;
-  for (let i = 0; i < 4; i++) {
+
+  // Calculate the number of marks depending on the size of the device
+  const numberOfMarks = 4;
+
+  const inc = ((metadata.StopTime - metadata.StartTime) / numberOfMarks) * 1000;
+  for (let i = 0; i < numberOfMarks; i++) {
     marks[t] = new Date(t).toLocaleString();
     t += inc;
   }
@@ -63,49 +78,117 @@ const TimeControl = ({
 
   return (
     <Flex
-      maxH={{base: "auto", lg: 10}}
       align="center"
       w="100%"
-      direction={{base: "column-reverse", lg: "row"}}
-      pb={{base: 50, lg: 0}}>
+      direction={{ base: "column-reverse", lg: "row" }}>
+      <Show below={"lg"}>
+        <Button w={"100%"} onClick={onOpen}>
+          Manage Time-Range
+        </Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modify Time Range</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Box
+                pt={10}
+                pb={10}
+                w={"100%"}
+                h={"50vh"}>
+                <Center w={"100%"} h={"100%"}>
+                <Range
+                  min={defaultTime[0]}
+                  max={defaultTime[1]}
+                  defaultValue={defaultTime}
+                  value={tooltipValues}
+                  draggableTrack={true}
+                  vertical
+                  
+                  onBeforeChange={() => setShowTooltip(true)}
+                  onChange={(val) => {
+                    setShowTooltip(true);
+                    if (metadata.IsRunning) {
+                      setTooltipValues([val[0], tooltipValues[1]]);
+                    } else {
+                      setTooltipValues(val);
+                    }
+                  }}
+                  onAfterChange={(val) => {
+                    setShowTooltip(false);
+                    setStartTime(new Date(val[0]));
+                    if (!metadata.IsRunning) {
+                      setStopTime(new Date(val[1]));
+                    }
+                  }}
+                  marks={marks}
+                  tipFormatter={(val) => new Date(val).toLocaleTimeString()}
+                  tipProps={{ visible: showTooltip }}
+                />
+                </Center>
+              </Box>
+              <Button
+                fontSize="sm"
+                onClick={() => {
+                  setStartTime(new Date(metadata.StartTime * 1000));
+                  setStopTime(new Date(metadata.StopTime * 1000));
+                }}
+                w={"100%"}
+              >
+                Reset Time Range
+              </Button>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </Show>
+      <Hide below={"lg"}>
+        <Box
+          mr={50}
+          ml={50}
+          mb={{
+            base: 10,
+            xl: 0
+          }}
+          w={"100%"}>
 
-      <Box mr={5} ml={{base: 5, lg: 0}} w={{base: "80%", lg: "100%"}}>
-        <Range
-          min={defaultTime[0]}
-          max={defaultTime[1]}
-          defaultValue={defaultTime}
-          value={tooltipValues}
-          draggableTrack={true}
-          onBeforeChange={() => setShowTooltip(true)}
-          onChange={(val) => {
-            setShowTooltip(true);
-            if (metadata.IsRunning) {
-              setTooltipValues([val[0], tooltipValues[1]]);
-            } else {
-              setTooltipValues(val);
-            }
+          <Range
+            min={defaultTime[0]}
+            max={defaultTime[1]}
+            defaultValue={defaultTime}
+            value={tooltipValues}
+            draggableTrack={true}
+            onBeforeChange={() => setShowTooltip(true)}
+            onChange={(val) => {
+              setShowTooltip(true);
+              if (metadata.IsRunning) {
+                setTooltipValues([val[0], tooltipValues[1]]);
+              } else {
+                setTooltipValues(val);
+              }
+            }}
+            onAfterChange={(val) => {
+              setShowTooltip(false);
+              setStartTime(new Date(val[0]));
+              if (!metadata.IsRunning) {
+                setStopTime(new Date(val[1]));
+              }
+            }}
+            marks={marks}
+            tipFormatter={(val) => new Date(val).toLocaleTimeString()}
+            tipProps={{ visible: showTooltip }}
+          />
+        </Box>
+        <Button
+          fontSize="sm"
+          onClick={() => {
+            setStartTime(new Date(metadata.StartTime * 1000));
+            setStopTime(new Date(metadata.StopTime * 1000));
           }}
-          onAfterChange={(val) => {
-            setShowTooltip(false);
-            setStartTime(new Date(val[0]));
-            if (!metadata.IsRunning) {
-              setStopTime(new Date(val[1]));
-            }
-          }}
-          marks={marks}
-          tipFormatter={(val) => new Date(val).toLocaleTimeString()}
-          tipProps={{ visible: showTooltip }}
-        />
-      </Box>
-      <Button
-        fontSize="sm"
-        onClick={() => {
-          setStartTime(new Date(metadata.StartTime * 1000));
-          setStopTime(new Date(metadata.StopTime * 1000));
-        }}
-      >
-        Reset Time Range
-      </Button>
+        >
+          Reset Time Range
+        </Button>
+      </Hide>
+
     </Flex>
   );
 };
