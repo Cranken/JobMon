27ab859c1cb1c7b13c8559d1baf10de04786d04a
Daniel Schild <daniel.schild@student.kit.edu>
2023-07-10 17:19:16 +0200

    Add toggles for changepoints quantiles and scaling

--- frontend/components/jobview/ViewControl.tsx
+++ frontend/components/jobview/ViewControl.tsx
@@ -1,4 +1,4 @@
-import { Button, Select, Stack, Text, Tooltip } from "@chakra-ui/react";
+import { Button, FormControl, FormLabel, Select, SimpleGrid, Stack, Switch, Text, Tooltip } from "@chakra-ui/react";
 import JSZip from "jszip";
 import React from "react";
 import { useCookies } from "react-cookie";
@@ -23,7 +23,7 @@ interface ControlProps {
   selectedMetrics: string[];
   setSelectedMetrics: (val: string[]) => void;
   showChangepoints: boolean
-  setShowChangepoints?: (b:boolean) => void;
+  setShowChangepoints?: (b: boolean) => void;
 }
 
 /**
@@ -67,8 +67,8 @@ export const ViewControl = ({
 }: ControlProps) => {
   const [, , removeCookie] = useCookies(["Authorization"]);
   return (
-    <Stack px={{base: 0, lg: 3}}>
-      <Stack direction={{base: "column", lg: "row"}} justify="space-between">
+    <Stack px={{ base: 0, lg: 3 }}>
+      <Stack direction={{ base: "column", lg: "row" }} justify="space-between">
         <MetricSelection
           metrics={jobdata.MetricData?.map((val) => val.Config) ?? []}
           selectedMetrics={selectedMetrics}
@@ -80,25 +80,43 @@ export const ViewControl = ({
           </Button>
         )}
       </Stack>
-      <Stack direction={{base: "column", lg: "row"}} gap={2}>
-        {setShowChangepoints ? (
-          <Tooltip label={"Changepoints indicate changes in your codes behavior"}>
-            <Button fontSize="sm" onClick={() => setShowChangepoints(!showChangepoints)}>
-              Toggle Changepoints
-            </Button>
-          </Tooltip>
-        ) : null}
-        {setShowQuantiles ? (
-          <Button
-            fontSize="sm"
-            onClick={() => setShowQuantiles(!showQuantiles)}
-          >
-            Toggle Quantile View
-          </Button>
-        ) : null}
-        <Button fontSize="sm" onClick={() => setAutoScale(!autoScale)}>
-          Toggle Automatic Scaling
-        </Button>
+
+      <Stack direction={{ base: "column", lg: "row" }} gap={2}>
+        <Tooltip label={"Changepoints indicate changes in your codes behavior"}>
+          <Stack flexGrow={1} direction="row" align="center">
+            <Text>Changepoints</Text>
+            <Switch
+              defaultChecked={showChangepoints}
+              onChange={(e) => {
+                { setShowChangepoints ? setShowChangepoints(e.target.checked) : null }
+              }}
+              isDisabled={setShowChangepoints ? false : true}
+            />
+          </Stack>
+        </Tooltip>
+        <Tooltip label={"Quantile View shows quantiles instead of the actual values"}>
+        <Stack flexGrow={1} direction="row" align="center">
+          <Text>Quantile View</Text>
+          <Switch
+            defaultChecked={showQuantiles}
+            onChange={(e) => {
+              { setShowQuantiles ? setShowQuantiles(e.target.checked) : null }
+            }}
+            isDisabled={setShowQuantiles ? false : true}
+          />
+        </Stack>
+        </Tooltip>
+        <Tooltip label={"By default the charts are scaled automatically, using the displayed values"}>
+        <Stack flexGrow={1} direction="row" align="center">
+          <Text>Automatic Scaling</Text>
+          <Switch
+            defaultChecked={autoScale}
+            onChange={(e) => {
+              setAutoScale(e.target.checked)
+            }}
+          />
+        </Stack>
+        </Tooltip>
         {sampleInterval && sampleIntervals && !jobdata.Metadata.IsRunning ? (
           <Stack direction="row" flexGrow={1} align="center" justify="end">
             <Text>Select sample interval in seconds:</Text>
