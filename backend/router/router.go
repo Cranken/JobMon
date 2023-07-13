e60d876b60d06a28d130d7c703b0435a16c64c02
Holger Obermaier <holgerob@gmx.de>
2023-07-13 14:04:34 +0200

    Check authorization to access node metrics

--- backend/router/router.go
+++ backend/router/router.go
@@ -256,8 +256,10 @@ func (r *Router) GetJob(
 	params httprouter.Params,
 	user auth.UserInfo) {
 
+	logging.Info("Router: GetJob(): Processing request: ", req.URL.String())
 	start := time.Now()
 
+	// Get request parameters
 	node := req.URL.Query().Get("node")
 	raw := req.URL.Query().Get("raw") == "true"
 	strId := params.ByName("id")
@@ -283,6 +285,21 @@ func (r *Router) GetJob(
 		logging.Error("router: GetJob(): User ", user.Username, " is not permitted to access job ", j.Id)
 		w.WriteHeader(http.StatusUnauthorized)
 		return
+	}
+
+	// Check authorization to access node metrics
+	if node != "" {
+		jobNodeMap := make(map[string]struct{})
+		for _, jobNode := range strings.Split(j.NodeList, "|") {
+			jobNodeMap[jobNode] = struct{}{}
+		}
+		for _, reqNode := range strings.Split(node, "|") {
+			if _, ok := jobNodeMap[reqNode]; !ok {
+				logging.Error("router: GetJob(): Node ", reqNode, "does not belong to job ", j.Id)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+		}
 	}
 
 	// Calculate best sample interval
@@ -410,7 +427,7 @@ func (r *Router) GetMetric(
 		})
 	if mc == -1 {
 		logging.Error("router: GetMetric(): Requested metric with GUID '", metric, "' not found")
-		w.WriteHeader(http.StatusUnauthorized)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -435,7 +452,9 @@ func (r *Router) GetMetric(
 	w.Write(jsonData)
 }
 
-// Search writes the search result to w, for the given request req, params and user.
+// Search uses http request parameter term as search term
+// When a job is found which matches this search term then job:<Job ID> is returned
+// Otherwise it is assumed that it is a username and user:<search term> is returned
 func (r *Router) Search(
 	w http.ResponseWriter,
 	req *http.Request,
@@ -1034,12 +1053,19 @@ func (r *Router) SetUserConfig(
 
 	w.Write(data)
 }
+
+// parseTag reads
+// * job ID from http request parameter job
+// * a tag from the http body
+// * job metadata from the PostgreSQL database
 func (r *Router) parseTag(
 	w http.ResponseWriter,
-	req *http.Request) (
+	req *http.Request,
+) (
 	job job.JobMetadata,
 	tag job.JobTag,
-	ok bool) {
+	ok bool,
+) {
 
 	jobStr := req.URL.Query().Get("job")
 
